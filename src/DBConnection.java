d80041cde08aba93bd6413655e88603ebdfd2155
ColmG12 <colmg12@hotmail.co.uk>
2018-11-19 01:47:00 +0000

    Added RadioPlayer and connected up to MariaDB

---
+++ src/DBConnection.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * Created by t00036478 on 01/02/2018.
+ */
+public class DBConnection {
+    private  String driverClass = "org.mariadb.jdbc.Driver";
+    private  Connection connection = null;
+    private  String url = "jdbc:mariadb://localhost:3306/oopprojecty2sem1";
+    private  String username = "root"; // your username
+    private  String password = ""; //your password
+    public DBConnection(){
+    }
+
+    private void setConnection() {
+        try {
+            Class.forName(driverClass).newInstance();
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+        } catch (Exception ex) {
+            System.err.println("Exception:"+ ex.getMessage());
+            ex.printStackTrace();
+        }
+
+    }
+    private Connection getConnection() {
+        if (connection == null) {
+            setConnection();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args){
+        DBConnection manager = new DBConnection();
+        manager.setConnection();
+
+    }
+}
--- src/PlaylistGUI.java
+++ src/PlaylistGUI.java
@@ -1,2 +1,54 @@
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+//import javax.swing.JPopupMenu;
+//import java.awt.FlowLayout;
+
 public class PlaylistGUI {
+    // frame
+    //JFrame f;
+    // Table
+    //JFrame j;
+    // Popup
+    //JPopupMenu p;
+
+    // Constructor
+    PlaylistGUI()
+    {
+        // Frame initiallization
+        JFrame radioframe = new JFrame();
+
+        // Frame Title
+        radioframe.setTitle("Playlist GUI");
+
+        // Data to be displayed in the JTable
+        String[][] data = {
+                { "Kundan Kumar Jha", "4031", "CSE" },
+                { "Anand Jha", "6014", "IT" }
+        };
+
+        // Column Names
+        String[] columnNames = { "Name", "Roll Number", "Department" };
+
+        // Initializing the JTable
+        JTable songtable= new JTable(data, columnNames);
+        songtable.setBounds(30, 40, 200, 300);
+
+        // adding it to JScrollPane
+        JScrollPane scroll = new JScrollPane(songtable);
+        radioframe.add(scroll);
+        // Frame Size
+        radioframe.setSize(1500, 600);
+        // Frame Visible = true
+        radioframe.setVisible(true);
+        //
+        //radioframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    // Driver  method
+    public static void main(String[] args)
+    {
+        new PlaylistGUI();
+    }
 }
